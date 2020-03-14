264d1f02774ef90eb9989f4b602e7d4e466d3bcb
Noah Gambee <noah@gambee.dev>
2020-03-13 19:54:19 -0600

    Export CardContainer and add deleteIdea prop

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './CardContainer.css';
 import IdeaCard from '../IdeaCard/IdeaCard';
 
-const CardContainer = (ideas) => {
+const CardContainer = ({ideas, deleteIdea}) => {
   let allIdeaCards = ideas.map(idea => {
-    return <IdeaCard idea={idea} />
+    return <IdeaCard idea={idea} deleteIdea={deleteIdea} />
   })
 
   return (
