a2c543c8e4b52735a086b011106a5ed600115d4f
Noah Gambee <noah@gambee.dev>
2020-03-13 19:28:53 -0600

    Create boilerplate App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
+import AddIdeaForm from '../AddIdeaForm/AddIdeaForm'
+import CardContainer from '../CardContainer/CardContainer'
 
 export const App = () => {
-  
+  const [ideas, addIdea] = useState({ideas: []});
+
+  return (
+    <main>
+      <h1>IdeaBox... with Hooks!</h1>
+      <AddIdeaForm />
+      <CardContainer />
+    </main>
+  )
 }
